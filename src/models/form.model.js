f511be3c97977d7c3eece8baa190a43844e6d360
Fabián SH <fabian@adminprime.com>
2023-06-19 01:23:36 -0600

    dayjs agregado

--- src/models/form.model.js
+++ src/models/form.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const moment = require('moment-timezone')
+const dayjs = require('dayjs')
 
 const formSchema = new mongoose.Schema({
   title: String,
@@ -31,16 +31,19 @@ const formSchema = new mongoose.Schema({
 
 formSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    const now = moment()
-    const startDate = moment.tz(returnedObject.startDate, 'America/Mexico_City')
-    const endDate = moment.tz(returnedObject.endDate, 'America/Mexico_City')
+    const now = dayjs()
+    const startDateUTC = dayjs(returnedObject.startDate)
+    const endDateUTC = dayjs(returnedObject.endDate)
+    const startDate = startDateUTC.subtract(6, 'hour')
+    const endDate = endDateUTC.subtract(6, 'hour')
 
     /* timezone test */
     console.log('NOW = ', now)
     console.log('START_DATE_ORIGINAL = ', returnedObject.startDate)
     console.log('END_DATE_ORIGINAL = ', returnedObject.endDate)
-    console.log('START_DATE_MOMENT = ', startDate)
-    console.log('END_DATE_MOMENT = ', endDate)
+    console.log('START_DATE_DAYJS = ', startDate)
+    console.log('END_DATE_DAYJS = ', endDate)
+    console.log('===================')
 
     let visible = null
     if (now >= startDate && now <= endDate) {
